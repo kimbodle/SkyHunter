5276add820cf13ac84d288602d7ddc5dcd652ab4
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-07-28 03:34:58 +0900

    Enemy1

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -5,12 +5,51 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
+
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 2;
+
+    ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        parent = GameObject.FindWithTag("SpawnAtRuntime").transform;
+        AddRigidbody();
+    }
+
+    private void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         //Debug.Log($"hit! by {other.gameObject.name}");
+        ProcessHit();
+        if(hitPoints < 1)
+        {
+            KillEnemy();
+        }
+
+    }
+    private void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints--;
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    private void KillEnemy()
+    {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
     }
+
+    
 }
