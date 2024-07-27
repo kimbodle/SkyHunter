1d2449bbb5c4e0725a01aaf1a5ca43b5b4039b78
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-07-27 17:50:17 +0900

    Player, Enemy VFX

--- Assets/Script/Collision Handler.cs
+++ Assets/Script/Collision Handler.cs
@@ -1,16 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(this.name + "--Collided rith--" + collision.gameObject.name);
-    }
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+        StartCrashSequemce();
     }
+
+    private void StartCrashSequemce()
+    {
+        crashVFX.Play();
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        GetComponent<PlayerControl>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
 }
---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    private void OnParticleCollision(GameObject other)
+    {
+        //Debug.Log($"hit! by {other.gameObject.name}");
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
